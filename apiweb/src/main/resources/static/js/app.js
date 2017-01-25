f081f70f1736849d7d6ecd91495f12f3e50c2766
prmorais13 <pauloroberto@natal.rn.gov.br>
2017-01-24 22:39:26 -0300

    terceiro commit - casa

--- apiweb/src/main/resources/static/js/app.js
+++ apiweb/src/main/resources/static/js/app.js
@@ -1,2 +1,12 @@
 //Criação do modulo principal da aplicação
 var appCliente = angular.module("appCliente", ['ngRoute']);
+
+//Configuração de rota (navegação entre as páginas)
+appCliente.config(function ($routeProvider, $locationProvider){
+	
+	$routeProvider
+		.when("/clientes",{templateUrl:'view/cliente.html',	controller:'clienteController'})
+		.otherwise({rediretTo:'/'});
+});
+
+$locationProvider.html5Mode(true);
